491b54ae8793adaa9adc412423264b382af4abd3
Jason A. <you@example.com>
2023-11-14 19:02:04 -0300

    @media nav menu adjust details

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//menu nav
+
 var menuExp = document.querySelector('#exp-icon')
 var menuSide = document.querySelector('.menu-lateral ul')
 var menuBtn = document.querySelector('.menu-lateral')
@@ -7,3 +9,18 @@ menuExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandir')
     menuBtn.classList.toggle('expandir')
 })
+
+
+//click selection
+
+var menuItem = document.querySelectorAll('.menu-li')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+    item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+menuItem.forEach((item)=>
+item.addEventListener('click', selectLink)
+)
